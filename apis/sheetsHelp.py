469cf80a1c0f2501c31ab5fd262b46152c069ac0
elw-arduino <minecraftisawsome007+github@gmail.com>
2018-12-19 17:19:01 -0500

    Added Convienence Methods to convert between A1 and array offsets

--- apis/sheetsHelp.py
+++ apis/sheetsHelp.py
@@ -6,7 +6,7 @@ __SheetData__ = {}
 
 # fetch data from sheets
 # USE getSheetsData to get result
-def fetchSheetsData(service, spreadsheetID = '1qEqy6Kzv0SOxZT6f_-mvhOaJKo-MQYFgA5A_1KyayPo', range = 'A2:D'):
+def fetchSheetsData(service, spreadsheetID = '1qEqy6Kzv0SOxZT6f_-mvhOaJKo-MQYFgA5A_1KyayPo', range = 'A2:F'):
 	# Fetch from sheets
 	result = service.spreadsheets().values().get(spreadsheetId=spreadsheetID, range=range).execute()
 	# Store fetch result in dictionary for access later
@@ -16,7 +16,7 @@ def fetchSheetsData(service, spreadsheetID = '1qEqy6Kzv0SOxZT6f_-mvhOaJKo-MQYFgA
 # if the result is not cached and service is provided it will be requested from google
 # if the result is not cached and service is not provided or is None then None will be returned
 # USE fetchSheetsData to refresh the cache for a sheet and range
-def getSheetsData(spreadsheetID='1qEqy6Kzv0SOxZT6f_-mvhOaJKo-MQYFgA5A_1KyayPo', range = 'A2:E', service = None):
+def getSheetsData(spreadsheetID='1qEqy6Kzv0SOxZT6f_-mvhOaJKo-MQYFgA5A_1KyayPo', range = 'A2:F', service = None):
 	cache_key = getDictionaryKey(spreadsheetID, range)
 	#If service == None then dont attempt to fetch sheets data
 	if service is not None:
@@ -27,7 +27,7 @@ def getSheetsData(spreadsheetID='1qEqy6Kzv0SOxZT6f_-mvhOaJKo-MQYFgA5A_1KyayPo', 
 def getDictionaryKey(spreadsheetID, range):
 	return str(spreadsheetID)+str(range)
 
-def setProcessedColumn(service, spreadsheetID = '1qEqy6Kzv0SOxZT6f_-mvhOaJKo-MQYFgA5A_1KyayPo', col = "E"):
+def setProcessedColumn(service, spreadsheetID = '1qEqy6Kzv0SOxZT6f_-mvhOaJKo-MQYFgA5A_1KyayPo', col = "F"):
 	if type(col) == type(1):
 		colId = col
 	else:
@@ -84,16 +84,120 @@ def getProcessedColumn(service, spreadsheetID = '1qEqy6Kzv0SOxZT6f_-mvhOaJKo-MQY
 	return responce["matchedDeveloperMetadata"][0]["developerMetadata"]["location"]["dimensionRange"]["startIndex"]
 
 def setProcessed(service, offset, range, spreadsheetID = '1qEqy6Kzv0SOxZT6f_-mvhOaJKo-MQYFgA5A_1KyayPo'):
-	# Get the column Leter for use in A1 notation
-	col = chr(ord('A')+ getProcessedColumn(service, spreadsheetID))
-	# Do regex search for the numbers in the range
-	match = search("[0-9]+", range)
-	A1 = ""
-	row = None
-	if match:
-		# calculate the A1 notation for the offset in the range
-		row = str(int(match.group()) + offset)
-		A1 = col + row
+	#calculate the A1 notation for the offset in the range
+	A1 = getA1FromOffset([getProcessedColumn(service, spreadsheetID),offset],range)
 	#make update request to sheets api
 	service.spreadsheets().values().update(spreadsheetId=spreadsheetID, range=A1, valueInputOption="USER_ENTERED", body={"values":[["=TRUE"]]}).execute()
-	print "Marked Row "+row+" as Processed"
+	print "Marked Row "+str(getOffsetFromA1(A1)[1])+" as Processed"
+
+#These methods convert between A1 notation and array offset
+
+def getOffsetFromA1(a1_target, origin=None):
+	"""
+	convert from A1 notation to offsets from specified offset
+	
+	a1_target - string - point in A1 notation to convert to offsets
+	origin - array of size 2 - index 0 being column and index 1 being row. The column can be a letter sequence or number where column A = 1.
+	origin - string - range/point in A1 notation to which the offset is relative to
+	If origin == None then the origin of A1 is used
+	If origin is a range it is parsed and used as offset
+	
+	return array of size 2 - index 0 being column and index 1 being row. The column can be a letter sequence or number where column A = 1.
+	
+	"""
+	col_start = 0
+	row_start = 0
+	result = [0,0]
+	if origin != None:
+		if type(origin) == type([]):
+			if len(origin) != 2:
+				# invalid origin could not convert
+				return None
+			if type(origin[0]) == type(""):
+				col_start = ord('A')-upper(origin[0])
+			elif type(origin[0]) == type(1):
+				col_start = origin[0]
+			else:
+				#invalid origin could not convert
+				return None
+			if type(origin[1]) != type(1):
+				#invalid origin could not convert
+				return None
+			else:
+				row_start = origin[1]
+		elif type(origin) == type(""):
+			match = search("[A-Z]+", origin)
+			if match:
+				col_start = ord('A')-ord(match.group().upper())
+			match = search("[1-9][0-9]*", origin)
+			if match:
+				row_start = int(match.group())
+		else:
+			#invalid origin could not convert
+			return None
+	if type(a1_target) != type(""):
+		#invalid target could not convert
+		return None
+	match = search("[A-Z]+",a1_target)
+	if not match:
+		#invalid notation could not convert
+		return None
+	result[0] = ord(match.group()) - ord("A") - col_start
+	match = search("[1-9][0-9]*",a1_target)
+	if not match:
+		#invalid notation could not convert
+		return None
+	result[1] = int(match.group())-row_start
+	
+	return result
+
+def getA1FromOffset(offset, origin=None):
+	"""
+	convert from offsets from specified offset to A1 notation
+	
+	offset - array of size 2 - index 0 being column and index 1 being row. The column can be a letter sequence or number where column A = 1.
+	origin - array of size 2 - index 0 being column and index 1 being row. The column can be a letter sequence or number where column A = 1.
+	origin - string - range/point in A1 notation to which the offset is relative to
+	If origin == None then the origin of A1 is used
+	If origin is a range it is parsed and used as offset
+	
+	return string in A1 notation
+	
+	"""
+	col_start = 0
+	row_start = 0
+	if origin != None:
+		if type(origin) == type([]):
+			if len(origin) != 2:
+				# invalid origin could not convert
+				return None
+			if type(origin[0]) == type(""):
+				col_start = ord('A')-upper(origin[0])
+			elif type(origin[0]) == type(1):
+				col_start = origin[0]
+			else:
+				#invalid origin could not convert
+				return None
+			if type(origin[1]) != type(1):
+				#invalid origin could not convert
+				return None
+			else:
+				row_start = origin[1]
+		elif type(origin) == type(""):
+			match = search("[A-Z]+", origin)
+			if match:
+				col_start = ord('A')-ord(match.group().upper())
+			match = search("[1-9][0-9]*", origin)
+			if match:
+				row_start = int(match.group())
+		else:
+			#invalid origin could not convert
+			return None
+	if type(offset) != type([]):
+		#invalid offset could not convert
+		return None
+	col = chr(ord("A") + offset[0] + col_start)
+	row = offset[1] + row_start
+	A1 = col + str(row)
+	
+	return A1
